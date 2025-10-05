a7321283dbdd6f7dd3188548df951172d126a96b
muranivaddepelli <muranivaddepalli78@gmail.com>
2025-10-05 19:20:30 +0530

    dedede

--- server/server.js
+++ server/server.js
@@ -28,13 +28,13 @@ const app = express();
 // CORS: allow your frontend URL
 app.use(
   cors({
-    origin: ["https://eashaop.com","https://eashaop.com", "http://localhost:5500", "http://127.0.0.1:5500", "http://127.0.0.1:5173", "http://localhost:5173"], // frontend URL
+    origin: ["https://eashaop.com","https://www.eashaop.com", "http://localhost:5500", "http://127.0.0.1:5500", "http://127.0.0.1:5173", "http://localhost:5173"], // frontend URL
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
 
-app.use("*", cors())
+// app.use("*", cors())
 
 app.use(
   "/api/appointments/razorpay/webhook",
