e6301194aa30f1240eb0464a66fa5684ee905291
muranivaddepelli <muranivaddepalli78@gmail.com>
2025-10-05 18:54:38 +0530

    Cors

--- server/server.js
+++ server/server.js
@@ -28,11 +28,13 @@ const app = express();
 // CORS: allow your frontend URL
 app.use(
   cors({
-    origin: [`${process.env.CLIENT_ORIGIN}`, "http://localhost:5500", "http://127.0.0.1:5500", "http://127.0.0.1:5173", "http://localhost:5173"], // frontend URL
+    origin: [process.env.CLIENT_ORIGIN, "http://localhost:5500", "http://127.0.0.1:5500", "http://127.0.0.1:5173", "http://localhost:5173"], // frontend URL
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
+
+app.use("*", cors())
 
 app.use(
   "/api/appointments/razorpay/webhook",
