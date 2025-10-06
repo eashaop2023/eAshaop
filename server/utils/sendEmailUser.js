8a9dc7bcdecc72b598bf47194b45687fedfe9c51
muranivaddepelli <muranivaddepalli78@gmail.com>
2025-10-06 11:58:58 +0530

    send email issue

--- server/utils/sendEmailUser.js
+++ server/utils/sendEmailUser.js
@@ -6,7 +6,8 @@ const sendEmail = async ({ email, subject, message }) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      service: process.env.SMTP_SERVICE, 
+      service: process.env.SMTP_SERVICE,
+      secure: false,
       auth: {
         user: process.env.SMTP_MAIL,
         pass: process.env.SMTP_PASSWORD,
