0faba4544a281325a0cb16ea0b973cbb1322e185
muranivaddepelli <muranivaddepalli78@gmail.com>
2025-10-07 09:23:29 +0530

    dfdfdf

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,7 +1,7 @@
 // utils/sendEmail.js
 const nodemailer = require("nodemailer");
 const { Resend } = require('resend');
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(re_YZs1N49t_8bwb4PrxZMmjdLnM9LEK39zi);
 
 resend.domains.create({ name: 'eashaop.com', customReturnPath: 'outbound' });
 
