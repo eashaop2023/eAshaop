6f8eebbbb47265c1e0c77a5b993d4b1cbe6a5b59
eashaop2023 <eashaop2023@gmail.com>
2025-11-18 09:28:19 +0530

    Add socket disconnect handler with reason

--- server/services/socketService.js
+++ server/services/socketService.js
@@ -48,7 +48,7 @@ function initSocket(server) {
     });
 
     socket.on('disconnect', () => {
-      console.log('Socket disconnected:', socket.id);
+      console.log(`Socket disconnected: ${socket.id}, Reason: ${reason}`);
     });
   });
 
